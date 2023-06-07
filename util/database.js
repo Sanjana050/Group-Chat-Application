c3ade50be66eb451df8ce0c8e6d1be07ce7a4194
Sanjana050 <127759681+Sanjana050@users.noreply.github.com>
2023-06-07 07:10:50 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,3 +1,3 @@
 const Sequelize=require('sequelize');
-const sequelize=new Sequelize('groupchatapp','root','Neha@5678',{dialect:'mysql',host:'localhost'})
+const sequelize=new Sequelize('groupchatapp','root','Neha@5678',{dialect:'mysql',host:'database-2.cxuthawotyly.us-east-1.rds.amazonaws.com'})
 module.exports=sequelize;
